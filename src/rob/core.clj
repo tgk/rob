f21914fb19fa5ee0b272a37871e7b787d76bd8c4
tgk <tgk@cs.au.dk>
2011-05-31 20:07:47 +0200

    Serving index.html

--- src/rob/core.clj
+++ src/rob/core.clj
@@ -3,7 +3,8 @@
   (:use ring.middleware.json-params)
   (:require [clj-json.core :as json])
   (:use ring.adapter.jetty)
-  (:import java.util.Random))
+  (:import java.util.Random)
+  (:use clojure.contrib.generic.functor))
 
 ; Game state and changer
 
@@ -19,16 +20,9 @@
 	    :goal [6 4]})
 (def game-state (atom (random-game-state)))
 
-(defn move [steps]
-  (fn [player-state]
-    (case (:direction player-state)
-	  :north (assoc player-state :y (+ (:y player-state) steps))
-	  :south (assoc player-state :y (- (:y player-state) steps))
-	  :east (assoc player-state :x (+ (:x player-state) steps))
-	  :west (assoc player-state :x (- (:x player-state) steps)))))
+; Playing cards
 
 (defn remove-nth [n coll] (concat (take n coll) (drop (inc n) coll)))
-
 (defn play-card [current-game-state player-id card-number]
   (let [player-state (get-in current-game-state [:players player-id])
 	card (nth (:deck player-state) card-number)
@@ -40,6 +34,16 @@
        :deck (conj remaining-cards (sample cards))
        :queue new-queue))))
 
+; Advancing time
+
+(defn move [steps]
+  (fn [player-state]
+    (case (:direction player-state)
+	  :north (assoc player-state :y (+ (:y player-state) steps))
+	  :south (assoc player-state :y (- (:y player-state) steps))
+	  :east (assoc player-state :x (+ (:x player-state) steps))
+	  :west (assoc player-state :x (- (:x player-state) steps)))))
+
 (def player-actions
      {:forward (move 1)
       :backward (move -1)
@@ -48,7 +52,7 @@
       :fast-forward (move 2)
       :fire identity})
 
-(comment defn perform-card-action [player-state]
+(comment defn perform-card-action [current-game-state player-id]
   (let [player-state (get-in current-game-state [:players player-id])
 	card (nth (:deck player-state) card-number)]
     (assoc-in current-game-state [:players player-id] ((player-actions (:type card)) player-state))))
@@ -58,12 +62,12 @@
 	top-card (first queue)
 	top-card-time (:time top-card)]
     (if (= 0 top-card-time)
-      :to-do
+      ((player-actions (:type top-card)) player-state)
       (assoc player-state
 	:queue (cons (assoc top-card :time (dec top-card-time)) (rest queue))))))
 
 (defn advance-time [current-game-state]
-  :not-implemented)
+  (assoc current-game-state :players (fmap advance-time-for-player (:players current-game-state))))
 
 ; Web-server magic
 
@@ -77,7 +81,7 @@
   {:status "success"})
 
 (defroutes handler
-  (GET "/" [] (json-response {:hello :world}))
+  (GET "/" [] (slurp "index.html"))
   (GET "/game-state/" [] (json-response @game-state))
   (POST "/player1/" [player-id card-number] (json-response (play-card-on-game-state player-id card-number))))
 
