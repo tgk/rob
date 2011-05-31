7610152bba32d009b6aeaedd7e796ddc2236aa87
tgk <tgk@cs.au.dk>
2011-05-31 19:51:51 +0200

    Initial game-state with deck manipulation

--- project.clj
+++ project.clj
@@ -1,4 +1,9 @@
 (defproject rob "1.0.0-SNAPSHOT"
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]])
+                 [org.clojure/clojure-contrib "1.2.0"]
+		 [ring "0.3.8"]
+		 [ring-json-params "0.1.0"]
+		 [compojure "0.6.3"]
+		 [clj-json "0.3.2"]]
+  :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]])
--- src/rob/core.clj
+++ src/rob/core.clj
@@ -1,1 +1,88 @@
-(ns rob.core)
+(ns rob.core
+  (:use compojure.core)
+  (:use ring.middleware.json-params)
+  (:require [clj-json.core :as json])
+  (:use ring.adapter.jetty)
+  (:import java.util.Random))
+
+; Game state and changer
+
+(def random (Random.))
+(defn sample [coll] (nth coll (.nextInt random (count coll))))
+(defn make-card [type] {:type type, :time 20})
+(def cards (map make-card [:forward :backward :turn-left :turn-right :fast-forward :fire]))
+(defn random-deck [] (for [i (range 5)] (sample cards)))
+(defn initial-player [x y]
+  {:x x, :y y, :direction :north, :queue [], :deck (random-deck)})
+(defn random-game-state []
+  {:players {0 (initial-player 0 3), 1 (initial-player 0 5)}
+	    :goal [6 4]})
+(def game-state (atom (random-game-state)))
+
+(defn move [steps]
+  (fn [player-state]
+    (case (:direction player-state)
+	  :north (assoc player-state :y (+ (:y player-state) steps))
+	  :south (assoc player-state :y (- (:y player-state) steps))
+	  :east (assoc player-state :x (+ (:x player-state) steps))
+	  :west (assoc player-state :x (- (:x player-state) steps)))))
+
+(defn remove-nth [n coll] (concat (take n coll) (drop (inc n) coll)))
+
+(defn play-card [current-game-state player-id card-number]
+  (let [player-state (get-in current-game-state [:players player-id])
+	card (nth (:deck player-state) card-number)
+	remaining-cards (remove-nth card-number (:deck player-state))
+	new-queue (conj (:queue player-state) card)]
+    (assoc-in
+     current-game-state [:players player-id]
+     (assoc player-state
+       :deck (conj remaining-cards (sample cards))
+       :queue new-queue))))
+
+(def player-actions
+     {:forward (move 1)
+      :backward (move -1)
+      :turn-left identity
+      :turn-right identity
+      :fast-forward (move 2)
+      :fire identity})
+
+(comment defn perform-card-action [player-state]
+  (let [player-state (get-in current-game-state [:players player-id])
+	card (nth (:deck player-state) card-number)]
+    (assoc-in current-game-state [:players player-id] ((player-actions (:type card)) player-state))))
+
+(defn advance-time-for-player [player-state]
+  (let [queue (:queue player-state)
+	top-card (first queue)
+	top-card-time (:time top-card)]
+    (if (= 0 top-card-time)
+      :to-do
+      (assoc player-state
+	:queue (cons (assoc top-card :time (dec top-card-time)) (rest queue))))))
+
+(defn advance-time [current-game-state]
+  :not-implemented)
+
+; Web-server magic
+
+(defn json-response [data & [status]]
+  {:status (or status 200)
+   :headers {"Content-Type" "application/json"}
+   :body (json/generate-string data)})
+
+(defn play-card-on-game-state [player-id card-number]
+  (swap! game-state play-card player-id card-number)
+  {:status "success"})
+
+(defroutes handler
+  (GET "/" [] (json-response {:hello :world}))
+  (GET "/game-state/" [] (json-response @game-state))
+  (POST "/player1/" [player-id card-number] (json-response (play-card-on-game-state player-id card-number))))
+
+(def app
+     (-> handler
+	 wrap-json-params))
+
+(future (run-jetty (var app) {:port 8080}))
