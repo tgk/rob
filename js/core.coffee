91df8f7ee898a5785157fa5093bf000e3adce401
Kasper Langer <kasperlanger@me.com>
2011-05-31 21:33:37 +0200

    work on html and coffeescript

--- js/core.coffee
+++ js/core.coffee
@@ -1,23 +1,37 @@
 SZ = 20
 angle = north: 0, east: 90, south: 180, west: 270
+labels = fire: "!", "turn-left": "&#8634", "turn-right": "&#8634", forward: "&uarr;", backward: "&darr;"
+whoami = 0
 
 $.fn.addPlayer = (data) ->
 	$('<div class="player"><div style="-webkit-transform: rotate('+angle[data.direction]+'deg); ">&uarr;</div></div>')
 		.css
-			bottom: SZ * data.x
-			left: SZ * data.y
+			bottom: SZ * data.y
+			left: SZ * data.x
 		.appendTo(@)
 
-x=0
 main = () ->
-	$('.board').empty()
-	  .addPlayer
-		  x:x
-		  y:3
-		  direction: "north"
+	$.get '/game-state', (state) ->
+		$('.board').empty()
+			.addPlayer(state.players[0])
+			.addPlayer(state.players[1])
+		
+		actions = $('.actions').empty()
+		$(state.players[whoami].deck).each (i) ->
+			$('<li/>').html(labels[@type] || @type)
+				.data('index', i)
+				.appendTo(actions)
 
 $ ->
-	$('.actions li').click (ev) ->
-		x++
-		ev.preventDefault()
+	console.debug("??")
+	$('.actions li').live 'mousedown', (ev) ->
+		i = $(@).data('index')
+		d = '{"player-id": ' + whoami + ', "card-number": ' + i + '}'
+		settings =
+			url: '/play-card'
+			type: "post"
+			contentType: 'application/json'
+			data: d
+		console.debug settings
+		$.ajax settings
 	setInterval main, 50
--- js/core.js
+++ js/core.js
@@ -1,5 +1,5 @@
 (function() {
-  var SZ, angle, main, x;
+  var SZ, angle, labels, main, whoami;
   SZ = 20;
   angle = {
     north: 0,
@@ -7,24 +7,44 @@
     south: 180,
     west: 270
   };
+  labels = {
+    fire: "!",
+    "turn-left": "&#8634",
+    "turn-right": "&#8634",
+    forward: "&uarr;",
+    backward: "&darr;"
+  };
+  whoami = 0;
   $.fn.addPlayer = function(data) {
     return $('<div class="player"><div style="-webkit-transform: rotate(' + angle[data.direction] + 'deg); ">&uarr;</div></div>').css({
-      bottom: SZ * data.x,
-      left: SZ * data.y
+      bottom: SZ * data.y,
+      left: SZ * data.x
     }).appendTo(this);
   };
-  x = 0;
   main = function() {
-    return $('.board').empty().addPlayer({
-      x: x,
-      y: 3,
-      direction: "north"
+    return $.get('/game-state', function(state) {
+      var actions;
+      $('.board').empty().addPlayer(state.players[0]).addPlayer(state.players[1]);
+      actions = $('.actions').empty();
+      return $(state.players[whoami].deck).each(function(i) {
+        return $('<li/>').html(labels[this.type] || this.type).data('index', i).appendTo(actions);
+      });
     });
   };
   $(function() {
-    $('.actions li').click(function(ev) {
-      x++;
-      return ev.preventDefault();
+    console.debug("??");
+    $('.actions li').live('mousedown', function(ev) {
+      var d, i, settings;
+      i = $(this).data('index');
+      d = '{"player-id": ' + whoami + ', "card-number": ' + i + '}';
+      settings = {
+        url: '/play-card',
+        type: "post",
+        contentType: 'application/json',
+        data: d
+      };
+      console.debug(settings);
+      return $.ajax(settings);
     });
     return setInterval(main, 50);
   });
