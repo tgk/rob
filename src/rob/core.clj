83c73149aefea9ec3d1960c88ec24c2841a32540
tgk <tgk@cs.au.dk>
2011-06-06 19:08:00 +0200

    Url with user ids added

--- src/rob/core.clj
+++ src/rob/core.clj
@@ -1,6 +1,8 @@
 (ns rob.core
   (:use compojure.core)
-  (:use ring.middleware.json-params)
+  (:use ring.util.response
+	ring.middleware.json-params
+	ring.middleware.keyword-params)
   (:require [clj-json.core :as json])
   (:use ring.adapter.jetty)
   (:import [java.util Random UUID]
@@ -16,16 +18,17 @@
 (defn random-deck [] (for [i (range 5)] (sample cards)))
 (defn initial-player [x y]
   {:x x, :y y, :direction :north, :queue [], :deck (random-deck)})
-(defn random-game-state []
-  {:players {}
-	    :goal [6 4]})
+(defn random-game-state [] {:players {} :goal [6 4]})
 
 (def game-state (atom (random-game-state)))
 
-(defn add-player []
+(defn add-player [current-game-state player-id]
+  (assoc-in current-game-state [:players player-id] (initial-player 0 0)))
+
+(defn add-uuid-player-to-game-state []
   (let [uuid (.toString (UUID/randomUUID))]
-    (swap! game-state assoc-in [:players uuid] (initial-player 0 0))
-    {:player-id uuid}))
+    (swap! game-state add-player uuid)
+    uuid))
 
 ; Playing cards
 
@@ -78,15 +81,32 @@
 	    :queue (rest queue))
 	  (assoc player-state
 	    :queue (cons (assoc top-card :time (dec top-card-time)) (rest queue))))))))
+
+(defn increment-card-time [card] (assoc card :time (inc (:time card))))
+(defn increment-deck-time [player]
+  (assoc player :deck
+	 (fmap increment-card-time (:deck player))))
     
 (defn advance-time [current-game-state]
-  (assoc current-game-state
-    :players (fmap advance-time-for-player (:players current-game-state))
-    :last-updated (System/currentTimeMillis)))
+  (let [players (:players current-game-state)
+	players (fmap advance-time-for-player players)
+	;players (fmap increment-deck-time players)
+	]
+    (assoc current-game-state
+      :players players
+      :last-updated (System/currentTimeMillis))))
 
 (def thread-pool (Executors/newScheduledThreadPool 4))
 (def scheduler
      (.scheduleAtFixedRate thread-pool (fn [] (swap! game-state advance-time)) 0 200 TimeUnit/MILLISECONDS))
+
+(defn tailored-game-state [current-game-state player-id]
+  (let [players (:players current-game-state)
+	me (get players player-id)
+	others (dissoc players player-id)]
+    (assoc
+	(dissoc current-game-state :players)
+      :me me, :others others)))
 
 ; Web-server magic
 
@@ -100,16 +120,20 @@
   {:status "success"})
 
 (defroutes handler
-  (GET "/" [] (slurp "index.html"))
+  (GET "/" [] (redirect "/add-player"))
   (GET "/js/core.js" [] (slurp "js/core.js"))
   (GET "/restart" [] (reset! game-state (random-game-state)))
-  (GET "/add-player" [] (json-response (add-player)))
-  (GET "/game-state" [] (json-response @game-state))
-  (POST "/play-card" [player-id card-number]
-	(json-response (play-card-on-game-state player-id card-number))))
+  (GET "/add-player" []
+       (redirect (format "/game/%s" (add-uuid-player-to-game-state))))
+  (GET "/game/:player-id" [] (slurp "index.html"))
+  (GET "/game/:player-id/game-state" [player-id]
+       (json-response (tailored-game-state @game-state player-id)))
+  (POST "/game/:player-id/:card-number" [player-id card-number]
+       (json-response (play-card-on-game-state player-id card-number))))
 
 (def app
      (-> handler
-	 wrap-json-params))
+	 wrap-json-params
+	 wrap-keyword-params))
 
 (future (run-jetty (var app) {:port 8080}))
